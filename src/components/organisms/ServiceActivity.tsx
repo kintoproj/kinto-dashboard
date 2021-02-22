de3ef3bcebe7969f4f22eeb5288a8154b8251678
Nandi Wong <nandi0315@gmail.com>
2021-02-22 12:31:59 +0800

    remove unused variable

--- src/components/organisms/ServiceActivity.tsx
+++ src/components/organisms/ServiceActivity.tsx
@@ -351,9 +351,6 @@ const ServiceActivity = ({ service, releaseIdToAction, ...props }: Props) => {
         key={release.getId()}
         hover={true}
         onClick={() => {
-          // catalog won't open the logs
-          const serviceType =
-            release?.getRunconfig()?.getType() || Block.Type.NOT_SET;
           if (releaseState === Status.State.REVIEW_DEPLOY) {
             dispatch(
               showPanel({
